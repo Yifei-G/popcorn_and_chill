b876aa41c63dd494eb09db9997bf60d6aa1c57ef
Yifei <yifei.ge91@gmail.com>
2021-02-22 23:52:59 +0100

    added digital platform information in movie detail page
    -Added the platform information in the detail page section
    -we also get the video provider information when loading the detail page

--- src/js/detail.js
+++ src/js/detail.js
@@ -3,6 +3,7 @@ import Request from "./request.js";
 const baseURL = Constants.basePath;
 const apiKey = Constants.apiKey;
 const imgBasePath = "https://image.tmdb.org/t/p/w342/";
+const logoBasePath = "https://image.tmdb.org/t/p/w45/";
 window.onload = function WindowLoad(event){
     const params = new URLSearchParams(window.location.search),
     movieID = params.get("movieID");
@@ -12,8 +13,15 @@ window.onload = function WindowLoad(event){
         const request = new Request();
         request.setBaseURL(baseURL);
         try{
-            const data = await request.loadMovies(`movie/${movieID}?${apiKey}&language=en-US`);
-            displayMovie(data);
+            const movieData = await request.loadMovies(`movie/${movieID}?${apiKey}&language=en-US`);
+            const providerData = await request.loadMovies(`movie/${movieID}/watch/providers?${apiKey}`);
+            console.log(providerData);
+            debugger;
+            displayMovie(movieData);
+            if(Object.keys(providerData.results).length > 0 && providerData.results.constructor === Object){
+                displayPlatforms(providerData.results);
+            }
+            
         }   catch(error){
             console.log(error);
         }
@@ -44,6 +52,32 @@ function displayMovie(movie){
     posterCtn.insertAdjacentHTML("beforeend", poster);
 
     infoCtn.insertAdjacentHTML("beforeend", primaryInfo);
+}
 
+function displayPlatforms(providers){
+    debugger;
+    const platformContainer = `<div class="platform-container d-flex-block flex-wrap border border-danger" ></div>`;
+    const infoCtn = document.querySelector(".info-container");
+    infoCtn.insertAdjacentHTML("beforeend", platformContainer);
+    const platformTitle = `<h2>Watch Now:</h2>`;
+    const platformCtn = document.querySelector(".platform-container");
+    platformCtn.insertAdjacentHTML("beforeend", platformTitle);
+    if(providers.US.rent){
+        const rentTitle = `<h3 class="mx-2">Buy:</h3>`;
+        platformCtn.insertAdjacentHTML("beforeend", rentTitle);
+        for(const rentPlatform of providers.US.rent){
+            debugger;
+            const platformLogo = `<img class="mx-2" src="${logoBasePath}${rentPlatform.logo_path}">`
+            platformCtn.insertAdjacentHTML("beforeend", platformLogo);
+        }
 
+    }
+    if(providers.US.flatrate){
+        const streamTitle = `<h3 class="mx-2 my-3">Stream:</h3>`;
+        platformCtn.insertAdjacentHTML("beforeend", streamTitle);
+        for(const steamPlatform of providers.US.flatrate){
+            const platformLogo = `<img class="mx-2" src="${logoBasePath}${steamPlatform.logo_path}">`
+            platformCtn.insertAdjacentHTML("beforeend", platformLogo);
+        }
+    }
 }
