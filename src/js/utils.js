08544c2c8f4a7e249d23e6b1172d2b83b89548c3
Yifei <yifei.ge91@gmail.com>
2021-03-01 01:54:21 +0100

    Update utils.js
    -we only display movies with posters

--- src/js/utils.js
+++ src/js/utils.js
@@ -4,12 +4,16 @@ function displayMovies(movies, containerID){
     for(const movie of movies){
         //each movie contain is also a clicable link to redirect to the detail page
         //let's generate the movie container div with the img element
-        const params = new URLSearchParams();
-        params.append("movieID", movie.id);
-        const movieContainer = `<a href="./detail.html?${params.toString()}"><div id="${movie.id}" class="movie-container">
-        <img class="poster-container rounded rounded-3" src=${coverImgPath}${movie.poster_path} alt="movie-cover">
-        </div></a>`;
-        targetContainer.insertAdjacentHTML("beforeend", movieContainer);
+        //we don't display movies without a poster
+        if(movie.poster_path){
+            const params = new URLSearchParams();
+            params.append("movieID", movie.id);
+            const movieContainer = `<a href="./detail.html?${params.toString()}"><div id="${movie.id}" class="movie-container">
+            <img class="poster-container rounded rounded-3" src=${coverImgPath}${movie.poster_path} alt="movie-cover">
+            </div></a>`;
+            targetContainer.insertAdjacentHTML("beforeend", movieContainer);
+    
+        }
 
     }
 
